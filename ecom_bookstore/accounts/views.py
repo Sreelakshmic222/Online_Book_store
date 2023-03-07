f0349844a6084606878e28091188dac939f9eb62
Sreelakshmic222 <sreelakshmic222@gmail.com>
2023-03-07 20:35:03 +0530

    user accounts and checkout

---
+++ ecom_bookstore/accounts/views.py
@@ -0,0 +1,44 @@
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from .forms import NewUserForm
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration Successful")
+            return redirect("accounts:login")
+        messages.error(request, "Unsuccessful registration.Invalid Information")
+    form = NewUserForm()
+    return render(request=request, template_name="accounts/register.html", context={"register_form": form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("list")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="accounts/login.html", context={"login_form": form})
+
+def home(request):
+    template="accounts/home.html"
+    context={}
+    return render(request,template,context)
