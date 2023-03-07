6605b8e28a94b622fc15d0f65b451f78d57c6805
Sreelakshmic222 <sreelakshmic222@gmail.com>
2023-03-07 12:46:33 +0530

    user accounts and checkout

--- ecom_bookstore/books/urls.py
+++ ecom_bookstore/books/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import BookListView,BookDetailView,BookCheckoutView
+from .views import BookListView,BookDetailView,BookCheckoutView,PaymentComplete
 urlpatterns=[
     path('book_list/',BookListView.as_view(),name='list'),
     path('details/<int:pk>/',BookDetailView.as_view(),name='detail-view'),
-path('checkout/<int:pk>/',BookCheckoutView.as_view(),name='checkout-view')
-
+    path('checkout/<int:pk>/',BookCheckoutView.as_view(),name='checkout'),
+    path('complete',PaymentComplete,name='complete')
 ]
